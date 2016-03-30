4bacf403ba2e392a072cd0865895bf4419404d4a
xang <xang.java@gmail.com>
2016-03-30 11:09:32 +0700

    new commit

---
+++ src/configdb/ConnectionConfig.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package configdb;
+
+/**
+ *
+ * @author xang-
+ */
+public interface ConnectionConfig {
+    
+    public static final String dbName="nuolmaker";
+     public static final String Username="root";
+      public static final String Password="";
+     public static final String ConnectUri="jdbc:mysql://localhost:3306/"+dbName;
+     
+     
+    
+}
---
+++ src/core/connectnuolmakerdb.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package core;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author xang-
+ */
+public class connectnuolmakerdb {
+    
+    
+ public Connection getconnection(){
+     
+     Connection con=null;
+     try {
+         con=(Connection) DriverManager.getConnection(configdb.ConnectionConfig.ConnectUri, configdb.ConnectionConfig.Username, configdb.ConnectionConfig.Password);
+    
+     return con;
+     } catch (SQLException ex) {
+         ex.printStackTrace();
+         return null;
+     }
+  
+ }//get connection db nuolmaker
+    
+     
+    
+}//class connection db
